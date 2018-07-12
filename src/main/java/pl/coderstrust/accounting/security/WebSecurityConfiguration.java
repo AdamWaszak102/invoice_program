1ddbfc4ab35de5b036cfa52f81a6339e34ad4a2d
AdamWaszak102 <waszakadam@gmail.com>
2018-07-12 14:19:03 +0200

    secutity fix

--- src/main/java/pl/coderstrust/accounting/security/WebSecurityConfiguration.java
+++ src/main/java/pl/coderstrust/accounting/security/WebSecurityConfiguration.java
@@ -1,5 +1,17 @@
 package pl.coderstrust.accounting.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
