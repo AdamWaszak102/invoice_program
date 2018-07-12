0993db470127349dab03d4390e90e5364b63d313
AdamWaszak102 <waszakadam@gmail.com>
2018-07-12 15:50:14 +0200

    email

--- src/main/java/pl/coderstrust/accounting/email/MailSenderConfig.java
+++ src/main/java/pl/coderstrust/accounting/email/MailSenderConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @Configuration
+@EnableScheduling
 public class MailSenderConfig {
 
   @Bean
